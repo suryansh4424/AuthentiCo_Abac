0fde71d279ecfb82674290c5bf40ec7fd8ca4ee7
Suryansh Rathore <131521058+suryansh4424@users.noreply.github.com>
2024-05-08 15:25:35 +0530

    1st commit

---
+++ api/views.py
@@ -0,0 +1,88 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project, Review, Tag
+from django.http import HttpResponseForbidden
+from functools import wraps
+
+def check_permission(resource_key, action_name, context):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            user_key = request.session.get('user_key')  # Extract user key from the request
+            
+            result = permit.check(
+                user_key=user_key,
+                resource_key=resource_key,
+                action=action_name,
+                context=context
+            )
+            
+            if not result.is_allowed():
+                return HttpResponseForbidden('Access denied')
+            
+            return view_func(request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
+
+
+@api_view(['GET'])
+def getRoutes(request):
+
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def removeTag(request):
+    tagId = request.data['tag']
+    projectId = request.data['project']
+
+    project = Project.objects.get(id=projectId)
+    tag = Tag.objects.get(id=tagId)
+
+    project.tags.remove(tag)
+
+    return Response('Tag was deleted!')
